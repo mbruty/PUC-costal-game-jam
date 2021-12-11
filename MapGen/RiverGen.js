071308e708737ffa76ccfd67e85f75e404d20085
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-12-11 17:04:45 +0000

    Add files via upload

--- MapGen/RiverGen.js
+++ MapGen/RiverGen.js
@@ -5,11 +5,16 @@ function RiverGen(map) {
 
   var rivers = Math.round(map.length / 300);
 
+  var Amax = 0;
+  var Amin = 5;
+  var Bmax = 5;
+  var Bmin = 95;
+
   for (j = 0; j < rivers; j++) {
     var randx = j * 300 + Math.floor(Math.random() * 300);
 
-    var A = Math.floor(Math.random() * 5);
-    var B = Math.floor(Math.random() * 100) / 100;
+    var A = Math.floor(Math.random() * (Amax - Amin + 1) + Amin);
+    var B = Math.floor(Math.random() * (Bmax - Bmin + 1) + Bmin) / 100;
     for (i = 0; i < map.length; i++) {
       var temp = Math.round(A * Math.sin(B * i));
       map[i][randx - 1 + temp] = 0;
