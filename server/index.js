a5b45533dac32a3c2afd5611fec3d6c395d4b469
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-12-11 09:50:46 +0000

    Created basic server boilerplate

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+const express = require("express");
+const path = require("path");
+const app = express();
+
+(async () => {
+  app.use(express.static(path.join(__dirname, "./public")));
+  await new Promise((resolve) => app.listen(8000, resolve));
+  console.log("🚀 Server blasting off at http://localhost:8000");
+})();
