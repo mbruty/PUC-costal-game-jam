d46e35e89a6cfff3b67cded5094e14aeddb5cf7a
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-12-11 11:08:49 +0000

    Add files via upload

--- MapGen/MapGen.js
+++ MapGen/MapGen.js
@@ -1,19 +1,20 @@
 const WNGen = require("./WNGen");
 
-//console.log(MapGen(10,10))
+//console.log(MapGen(1,1))
 
 function MapGen(x, y)
 {
+    const ChunkSize = 16;
     var map = [];
-    var Temp = WNGen(64,64);
+    var Temp = WNGen(ChunkSize,ChunkSize);
 
-    for(let i = 0; i < 64; i++)
+    for(let i = 0; i < ChunkSize; i++)
     {
         map[i] = [];
-        for(let j = 0; j < 64; j++)
+        for(let j = 0; j < ChunkSize; j++)
         {
             rng = Math.random();
-            if(rng * (i/16) < 0.5 )
+            if(rng * (i/ChunkSize) < 0.5 )
             {
                 map[i][j] = 0;
             }
