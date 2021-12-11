f4013631b2fee6e93a6db629bce31c7a97aea2fd
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-12-11 11:53:56 +0000

    Add files via upload

---
+++ MapGen/BlurFunc.js
@@ -0,0 +1,46 @@
+function BlurFunc(map) {
+  var N, S, E, W;
+
+  for (i = 0; i < map.length; i++) {
+    for (j = 0; j < map[i].length; j++) {
+      //if water
+      if (map[i][j] == 0) {
+        var sum = 0;
+        //check environment
+        if (j != 0) {
+          N = map[i][j - 1];
+        }
+        if (j != map.length - 1) {
+          S = map[i][j + 1];
+        }
+        if (i != 0) {
+          E = map[i - 1][j];
+        }
+        if (i != map.length - 1) {
+          W = map[i + 1][j];
+        }
+
+        //checkBlur
+        if (N == 1) {
+          sum = sum + 1;
+        }
+        if (S == 1) {
+          sum = sum + 1;
+        }
+        if (E == 1) {
+          sum = sum + 1;
+        }
+        if (W == 1) {
+          sum = sum + 1;
+        }
+        //setBlur
+        if (sum > 2) {
+          map[i][j] = 3;
+        }
+      }
+    }
+  }
+  return map;
+}
+
+module.exports = BlurFunc;
--- MapGen/WNGen.js
+++ MapGen/WNGen.js
@@ -1,17 +1,14 @@
 //console.log(WNGen(10,10));
-function WNGen(x, y)
-{
-    var map = [];
+function WNGen(x, y) {
+  var map = [];
 
-    for(let i = 0; i < x; i++)
-    {
-        map[i] = [];
-        for(let j = 0; j < y; j++)
-        {
-            map[i][j] = Math.random();
-        }
+  for (let i = 0; i < x; i++) {
+    map[i] = [];
+    for (let j = 0; j < y; j++) {
+      map[i][j] = Math.random();
     }
-    return map;
+  }
+  return map;
 }
 
 module.exports = WNGen;
