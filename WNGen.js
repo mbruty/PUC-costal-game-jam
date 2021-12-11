a41622efbcae30bf69fd82e94eb85aad72aaf92c
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-12-11 10:54:31 +0000

    MapGen
    Created Map Generator

---
+++ WNGen.js
@@ -0,0 +1,17 @@
+//console.log(WNGen(10,10));
+function WNGen(x, y)
+{
+    var map = [];
+
+    for(let i = 0; i < x; i++)
+    {
+        map[i] = [];
+        for(let j = 0; j < y; j++)
+        {
+            map[i][j] = Math.random();
+        }
+    }
+    return map;
+}
+
+module.exports = WNGen;
